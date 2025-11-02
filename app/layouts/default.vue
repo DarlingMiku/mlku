b7fbd9d65f8f8b26f77dab1516b9282daa04b623
Mlku <stonyaleks@gmail.com>
2025-11-02 03:06:02 +0300

    main:v0 init commit nuxt 4

---
+++ app/layouts/default.vue
@@ -0,0 +1,50 @@
+<script lang="ts" setup>
+const hasError = !!useSlots().error as any
+</script>
+
+<template>
+    <NuxtLoadingIndicator />
+    <slot
+        v-if="hasError"
+        name="error"
+    />
+    <NuxtPage v-else />
+</template>
+
+<style lang="scss">
+html,
+body {
+    height: 100%;
+    width: 100%;
+}
+
+body {
+    // font-family: "Geometria";
+    margin: 0;
+    padding: 0;
+    height: 100%;
+
+    & > #__nuxt {
+        display: flex;
+        flex-direction: column;
+        min-height: 100%;
+
+        & > main {
+            flex: 1;
+        }
+    }
+}
+
+.container {
+    display: flex;
+    max-width: 1440px;
+    width: 100%;
+    margin: 0 auto;
+    box-sizing: border-box;
+    padding: 0 40px;
+
+    @media (max-width: 1000px) {
+        padding: 0 20px;
+    }
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,49 @@
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+  telemetry: { enabled: false },
+  app: {
+    head: {
+      htmlAttrs: { lang: "ru" },
+      titleTemplate: "MIKU",
+      meta: [
+        { charset: "utf-8" },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1, user-scalable=no",
+        },
+        { "http-equiv": "Content-Type", content: "text/html; charset=UTF-8" },
+      ],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        { rel: "mask-icon", href: "/favicon.svg" },
+      ],
+    },
+  },
+  devServer: {
+    host: "0.0.0.0",
+    port: 8000,
+  },
+  modules: ["@nuxtjs/device"],
+  experimental: {
+    defaults: {
+      nuxtLink: {
+        trailingSlash: "append",
+      },
+    },
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "/assets/styles/main.scss" as *;',
+        },
+      },
+    },
+  },
+  postcss: {
+    plugins: {
+      autoprefixer: {},
+    },
+  },
+  compatibilityDate: "2025-08-02",
+});
